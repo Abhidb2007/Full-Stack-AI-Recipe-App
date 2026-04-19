5514d193d4ae162aaa684959bfb0925ee42372a4
Abhidb2007 <abhishekdb2210@gmail.com>
2026-04-19 21:23:49 +0530

    added code

---
+++ backend/src/api/saved-recipe/controllers/saved-recipe.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * saved-recipe controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::saved-recipe.saved-recipe');
---
+++ backend/src/api/saved-recipe/routes/saved-recipe.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * saved-recipe router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::saved-recipe.saved-recipe');
